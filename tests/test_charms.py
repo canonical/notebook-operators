1d6472d15bfc4689ab307e79d82626c637cd390d
Andrew Scribner <ca.scribner+1@gmail.com>
2022-09-07 09:18:26 -0400

    chore: Bump image versions (#56)
    * chore: Bump image versions
    
    * fix: add trust to dashboard deployment in integration tests (dashboard is sidecar charm)
    
    * feat: add notebook status and transition time in CRDs
    
    * feat: add patch to Role rules for events
    
    * Remove profile config for dashboard in tests
    
    The profile config option was removed in the dashboard currently in
    latest/edge.
    
    Co-authored-by: misohu <michal.hucko@canonical.com>
    Co-authored-by: Daniela Plascencia <daniela.plascencia@canonical.com>
    Co-authored-by: Dominik Fleischmann <dominik.fleischmann@canonical.com>

--- tests/test_charms.py
+++ tests/test_charms.py
@@ -110,7 +110,7 @@ async def test_build_and_deploy(ops_test, lightkube_client, dummy_resources_for_
     await ops_test.model.deploy(controller_charm, resources={"oci-image": controller_image_path})
     await ops_test.model.deploy("admission-webhook", channel="latest/edge")
     await ops_test.model.deploy("kubeflow-profiles", channel="latest/edge")
-    await ops_test.model.deploy("kubeflow-dashboard", channel="latest/edge", config={"profile": PROFILE_NAME})
+    await ops_test.model.deploy("kubeflow-dashboard", channel="latest/edge", trust=True)
     await ops_test.model.add_relation("kubeflow-profiles", "kubeflow-dashboard")
 
     # Wait for everything to deploy
