35e37d1028f67905ce6ece5f9f05436b709ee49d
Andrew Scribner <ca.scribner+1@gmail.com>
2022-12-02 16:29:54 -0500

    feat: update lint settings to match other charms

--- charms/jupyter-ui/tests/test_operator.py
+++ charms/jupyter-ui/tests/test_operator.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+"""Tests for the jupyter UI."""
 import pytest
 import yaml
 from charm import Operator
@@ -7,21 +11,25 @@ from ops.testing import Harness
 
 @pytest.fixture
 def harness():
+    """Instantiate a test harness."""
     return Harness(Operator)
 
 
 def test_not_leader(harness):
+    """Test that charm waits if not leader."""
     harness.begin_with_initial_hooks()
     assert harness.charm.model.unit.status == WaitingStatus("Waiting for leadership")
 
 
 def test_missing_image(harness):
+    """Test if charm is blocked if missing oci-image."""
     harness.set_leader(True)
     harness.begin_with_initial_hooks()
     assert harness.charm.model.unit.status == BlockedStatus("Missing resource: oci-image")
 
 
 def test_no_relation(harness):
+    """Test charm goes to active if no additional relations exist."""
     harness.set_leader(True)
     harness.add_oci_resource(
         "oci-image",
@@ -37,6 +45,7 @@ def test_no_relation(harness):
 
 
 def test_with_relation(harness):
+    """Test that charm goes to active if it has an istio-pilot relation."""
     harness.set_leader(True)
     harness.add_oci_resource(
         "oci-image",
