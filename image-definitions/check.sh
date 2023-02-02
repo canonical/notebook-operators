9dc93db71651f729cc3856ac7f3e988c0c31a7b0
Ivan Chvets <ivan.chvets@canonical.com>
2023-02-02 15:12:47 -0500

    feat: added check for commits https://github.com/canonical/notebook-operators/issues/181
    Summary of changes:
    - Added check for commit descrepancy between repository and the patch.

---
+++ image-definitions/check.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+#
+# Check if repositories were updates since patch and log it
+#
+# Usage: check.sh
+#
+
+# Kubeflow repository check
+echo "Check Kubeflow repository"
+REPO_DIR="kubeflow"
+cd $REPO_DIR
+KF_REPO_COMMIT=$(eval "git rev-parse --short HEAD")
+KF_PATCH_COMMIT=$(eval "cat ./kubeflow-patch-commit.txt")
+if [ $KF_REPO_COMMIT != KF_PATCH_COMMIT ]; then
+    echo "WARNING: Repository $REPO_DIR/ has been updated since patch was generated"
+    echo "Latest commit $KF_REPO_COMMIT Patch commit $KF_PATCH_COMMIT"
+fi
+
+# End of Kubeflow repository check
