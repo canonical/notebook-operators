8606d95fd6fa9047f68a43cac17c0f98f2ab2132
Ivan Chvets <ivan.chvets@canonical.com>
2023-02-07 14:24:14 -0500

    feat: re-designed check commmit https://github.com/canonical/notebook-operators/issues/181
    Summary of changes:
    - Redesigned update check to return error for commit difference.

--- image-definitions/check-update.sh
+++ image-definitions/check-update.sh
@@ -4,7 +4,7 @@
 #
 # Check if repositories were updates since patch and log it
 #
-# Usage: check.sh
+# Usage: check-update.sh
 #
 
 # Kubeflow repository check
@@ -14,9 +14,13 @@ cd $REPO_DIR
 KF_REPO_COMMIT=$(eval "git rev-parse --short HEAD")
 KF_PATCH_COMMIT=$(eval "cat ../kubeflow-patch-commit.txt")
 if [ $KF_REPO_COMMIT != KF_PATCH_COMMIT ]; then
-    echo "WARNING: Repository $REPO_DIR/ has been updated since patch was generated"
+    echo "Repository $REPO_DIR/ has been updated since patch was generated"
     echo "Latest commit $KF_REPO_COMMIT Patch commit $KF_PATCH_COMMIT"
+    false
 fi
 
 cd -
 # End of Kubeflow repository check
+
+# all checks have passed
+true
