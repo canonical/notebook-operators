f589b4a8f4761c597e33c91283f385d77e891099
i-chvets <113444075+i-chvets@users.noreply.github.com>
2023-02-10 10:54:43 -0500

    Fix check update script (#199)
    * fix: fixed incorrect setup call
    
    Summary of changes:
    - Fixed incorrect setup.sh script call.
    
    * fix: check-update.sh
    
    Summary of changes:
    - Fixed variable reference in check-update.sh

--- image-definitions/check-update.sh
+++ image-definitions/check-update.sh
@@ -13,7 +13,7 @@ REPO_DIR="kubeflow"
 cd $REPO_DIR
 KF_REPO_COMMIT=$(eval "git rev-parse --short HEAD")
 KF_PATCH_COMMIT=$(eval "cat ../kubeflow-patch-commit.txt")
-if [ $KF_REPO_COMMIT != KF_PATCH_COMMIT ]; then
+if [ $KF_REPO_COMMIT != $KF_PATCH_COMMIT ]; then
     echo "Repository $REPO_DIR/ has been updated since patch was generated"
     echo "Latest commit $KF_REPO_COMMIT Patch commit $KF_PATCH_COMMIT"
     exit 1
