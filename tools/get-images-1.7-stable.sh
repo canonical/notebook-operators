3c18e3a71238b09a2955c36efa9a3e549cc36b56
Ivan Chvets <ivan.chvets@canonical.com>
2023-07-12 13:34:30 -0400

    feat: added metadata images extraction

--- tools/get-images-1.7-stable.sh
+++ tools/get-images-1.7-stable.sh
@@ -6,11 +6,12 @@
 STATIC_IMAGE_LIST=(
 )
 # dynamic list
-git checkout origin/track/1.7
+#git checkout origin/track/1.7
 IMAGE_LIST=()
-IMAGE_LIST+=($(yq '.spawnerFormDefaults | .image | .options | values[]' charms/jupyter-ui/src/spawner_ui_config.yaml))
-IMAGE_LIST+=($(yq '.spawnerFormDefaults | .imageGroupOne | .options | values[]' charms/jupyter-ui/src/spawner_ui_config.yaml))
-IMAGE_LIST+=($(yq '.spawnerFormDefaults | .imageGroupTwo | .options | values[]' charms/jupyter-ui/src/spawner_ui_config.yaml))
+IMAGE_LIST+=($(find $REPO -type f -name metadata.yaml -exec yq '.resources | to_entries | .[] | .value | ."upstream-source"' {} \;))
+IMAGE_LIST+=($(yq '.spawnerFormDefaults | .image | .options | .[]' charms/jupyter-ui/src/spawner_ui_config.yaml))
+IMAGE_LIST+=($(yq '.spawnerFormDefaults | .imageGroupOne | .options | .[]' charms/jupyter-ui/src/spawner_ui_config.yaml))
+IMAGE_LIST+=($(yq '.spawnerFormDefaults | .imageGroupTwo | .options | .[]' charms/jupyter-ui/src/spawner_ui_config.yaml))
 
 printf "%s\n" "${STATIC_IMAGE_LIST[@]}"
 printf "%s\n" "${IMAGE_LIST[@]}"
