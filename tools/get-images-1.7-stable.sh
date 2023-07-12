0b7513165bdc114a033ed2e1a023e4115f103fbd
i-chvets <113444075+i-chvets@users.noreply.github.com>
2023-07-12 17:35:58 -0400

    Kf 3784 feat get images (#263)
    * feat: get all images managed by workload
    
    Repository specific list of images.
    Used by CVE scanning workflows and can be used to collect images URL for airgapped setup.
    
    Summary of changes:
    - Script that produces list of all workload managed container images that are related to this charm's workload.

--- tools/get-images-1.7-stable.sh
+++ tools/get-images-1.7-stable.sh
@@ -6,7 +6,7 @@
 STATIC_IMAGE_LIST=(
 )
 # dynamic list
-#git checkout origin/track/1.7
+git checkout origin/track/1.7
 IMAGE_LIST=()
 IMAGE_LIST+=($(find $REPO -type f -name metadata.yaml -exec yq '.resources | to_entries | .[] | .value | ."upstream-source"' {} \;))
 IMAGE_LIST+=($(yq '.spawnerFormDefaults | .image | .options | .[]' charms/jupyter-ui/src/spawner_ui_config.yaml))
