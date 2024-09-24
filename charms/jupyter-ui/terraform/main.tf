1e126e4a769dc27fe3f4e9abd8a55675a762c50a
Orfeas Kourkakis <orfeas.kourkakis@canonical.com>
2024-09-24 15:06:29 +0300

    feat: Add terraform modules for jupyter-{controller,ui} (#407)
    Create a `terraform/` directory for each of the charms that hosts their individual Terraform modules. It follows the structure proposed in [this spec](https://docs.google.com/document/d/1EG71A2pJ244PQRaGVzGj7Mx2B_bzE4U_OSqx4eeVI1E/edit) and it is based on what was done in canonical/argo-operators#198.
    
    Ref #405
    Ref #404

---
+++ charms/jupyter-ui/terraform/main.tf
@@ -0,0 +1,13 @@
+resource "juju_application" "jupyter_ui" {
+  charm {
+    name     = "jupyter-ui"
+    channel  = var.channel
+    revision = var.revision
+  }
+  config    = var.config
+  model     = var.model_name
+  name      = var.app_name
+  resources = var.resources
+  trust     = true
+  units     = 1
+}
