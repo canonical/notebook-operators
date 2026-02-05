3520ce45f569195544ef832020cbd7ebafa486fc
NohaIhab <noha.ihab@canonical.com>
2026-02-05 18:26:32 +0200

    chore: update terraform outputs to include ambient mesh relations

--- charms/jupyter-controller/terraform/outputs.tf
+++ charms/jupyter-controller/terraform/outputs.tf
@@ -4,13 +4,17 @@ output "app_name" {
 
 output "provides" {
   value = {
-    metrics_endpoint  = "metrics-endpoint",
     grafana_dashboard = "grafana-dashboard",
+    metrics_endpoint  = "metrics-endpoint",
+    provide_cmr_mesh  = "provide-cmr-mesh"
   }
 }
 
 output "requires" {
   value = {
-    logging = "logging"
+    gateway_metadata = "gateway-metadata",
+    logging          = "logging",
+    require_cmr_mesh = "require-cmr-mesh",
+    service_mesh     = "service-mesh"
   }
 }
